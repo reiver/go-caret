037cf6951ff038ae7719b21face1e7147af2decf
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-07-25 18:10:51 -0700

    docs

---
+++ doc.go
@@ -0,0 +1,11 @@
+/*
+Package caret encodes, and decodes caret text (i.e., where ASCII control codes are in caret notation) into UTF-8 text (which may also be ASCII text).
+
+This can be useful for writing, or reading text that contains ANSI escape codes, such as ANSI color codes.
+
+Note that Caret Notation code note have a good way of representing the caret in the output text.
+
+This is a limit in inherent to Caret Notation.
+
+*/
+package caret
